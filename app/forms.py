6c6a6318be1a896b97e282e313b8c1f14b7a383b
人見悠斗 <haruto.77.512@gmail.com>
2025-04-11 11:43:54 +0900

    途中

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,8 @@ class BS4ScheduleForm(forms.ModelForm):
 
     class Meta:
         model = Schedule
-        fields = ('summary', 'description', 'start_time', 'end_time')
+        fields = ('summary', 'description', 'start_date', 'end_date', 'start_time', 'end_time')
+
         widgets = {
             'summary': forms.TextInput(attrs={
                 'class': 'form-control',
@@ -15,22 +16,54 @@ class BS4ScheduleForm(forms.ModelForm):
             'description': forms.Textarea(attrs={
                 'class': 'form-control',
             }),
+            'start_date': forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date',
+            }),
+            'end_date': forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date',
+            }),
             'start_time': forms.TextInput(attrs={
                 'class': 'form-control',
+                'readonly': 'readonly',
             }),
             'end_time': forms.TextInput(attrs={
                 'class': 'form-control',
             }),
         }
 
-    def clean_end_time(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # start_dateをreadonlyに設定して変更不可にする
+        if self.instance and self.instance.start_date:
+            self.fields['start_date'].widget.attrs['readonly'] = 'readonly'
+            self.fields['start_date'].widget.attrs['class'] = 'form-control'
+            self.fields['start_date'].disabled = True  # disabledでフォーム送信時に値を送信しないようにする
+
+    def clean_end_date_time(self):
         start_time = self.cleaned_data['start_time']
         end_time = self.cleaned_data['end_time']
-        if end_time <= start_time:
+        start_date = self.cleaned_data['start_date']
+        end_date = self.cleaned_data['end_date']
+        if end_date < start_date:
+            raise forms.ValidationError(
+                '終了日は、開始日よりも後にしてください'
+            )
+        if end_date == start_date and end_time <= start_time:
             raise forms.ValidationError(
                 '終了時間は、開始時間よりも後にしてください'
             )
         return end_time
+    
+    def clean_end_date(self):
+        start_date = self.cleaned_data['start_date']
+        end_date = self.cleaned_data['end_date']
+        if end_date < start_date:
+            raise forms.ValidationError(
+                '終了日は、開始日よりも後にしてください'
+            )
+        return end_date
 
 
 class SimpleScheduleForm(forms.ModelForm):
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from datetime import timedelta
 import datetime
 from django.shortcuts import redirect, render
 from django.views import generic
@@ -5,16 +7,38 @@ from .forms import BS4ScheduleForm, SimpleScheduleForm
 from .models import Schedule
 from . import mixins
 
-
-
 class MonthCalendar(mixins.MonthCalendarMixin, generic.TemplateView):
     """月間カレンダーを表示するビュー"""
     template_name = 'app/month.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        calendar_context = self.get_month_calendar()
-        context.update(calendar_context)
+
+        week_calendar_context = self.get_week_calendar()
+        month_calendar_context = self.get_month_calendar()
+        context.update(week_calendar_context)
+        context.update(month_calendar_context)
+
+        # スケジュールの再構築（end_date 跨ぎ対応）
+        week_first = context['week_first']
+        week_last = context['week_last']
+
+        # スケジュール取得：その週に1日でもかかっているもの
+        schedules = Schedule.objects.filter(
+            end_date__gte=week_first,
+            start_date__lte=week_last,
+        )
+
+        # スケジュールを日付ごとに分配
+        week_day_schedules = defaultdict(list)
+        for schedule in schedules:
+            current = schedule.start_date
+            while current <= schedule.end_date:
+                if week_first <= current <= week_last:
+                    week_day_schedules[current].append(schedule)
+                current += timedelta(days=1)
+
+        context['week_day_schedules'] = week_day_schedules
         return context
 
 
@@ -62,6 +86,17 @@ class MyCalendar(mixins.MonthCalendarMixin, mixins.WeekWithScheduleMixin, generi
     model = Schedule
     date_field = 'date'
     form_class = BS4ScheduleForm
+
+    def get_initial(self):
+        initial = super().get_initial()
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        day = self.kwargs.get('day')
+        if year and month and day:
+            target_date = datetime.date(int(year), int(month), int(day))
+            initial['start_date'] = target_date
+            initial['end_date'] = target_date
+        return initial
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
