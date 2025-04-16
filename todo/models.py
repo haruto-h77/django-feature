9b0694632dc234004e40f3d3b75c2ea29fa2c3a0
shiraki <taisei5299@gmail.com>
2025-04-16 13:52:59 +0900

    アコーディオンとして概要追加

--- todo/models.py
+++ todo/models.py
@@ -5,12 +5,14 @@ from django.utils import timezone
 class Todo(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     item_name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
     registration_date = models.DateTimeField(blank=True,null=True)
     finished_date = models.DateTimeField(blank=True,null=True)
     expire_datetime = models.DateTimeField(blank=True,null=True)
     is_deleted = models.BooleanField(default=False)
     create_date_time = models.DateTimeField(default=timezone.now)
     update_date_time = models.DateTimeField(default=timezone.now)
+
 
     def publish(self):
         self.save()
